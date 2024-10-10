6396aad72271b058f24a95a2d73b775b2b701768
Lucas Toretta <lucastoretta@hotmail.com>
2024-10-09 23:48:49 -0300

    Add a branch

---
+++ main.js
@@ -0,0 +1,26 @@
+const form = document.getElementById ('Form-enviar');
+
+function validaNumero(NumeroB) {
+    return NumeroB.length > NumeroA
+}
+
+form.addEventListener('submit',function(e) {
+    let FormEValido = false;
+    e.preventDefault();
+
+    const NumeroA = document.getElementById('NumeroA');
+    const NumeroB = document.getElementById('NumeroB');
+    const mensagemSucesso = 'Tudo certo';
+
+    FormEValido = validaNumero(NumeroB.value)
+if (FormEValido) {
+    alert(mensagemSucesso);
+
+    NumeroA.value = '';
+    NumeroB.value = '';
+} else {
+    alert('O segundo número precisa ser maior que o primeiro');
+    }
+})
+
+console.log (form)
