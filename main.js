2674b37cb63a47b91476422056d50aadda38d6b2
Lucas Toretta <lucastoretta@hotmail.com>
2024-10-18 11:27:11 -0300

    alteracao Final

--- main.js
+++ main.js
@@ -1,26 +1,21 @@
+
 const form = document.getElementById ('Form-enviar');
 
-function validaNumero(NumeroB) {
-    return NumeroB.length > NumeroA
-}
-
 form.addEventListener('submit', function(e) {
-    let FormEValido = false;
     e.preventDefault();
 
     const NumeroA = document.getElementById('NumeroA');
     const NumeroB = document.getElementById('NumeroB');
-    const mensagemSucesso = 'Tudo certo';
+    const mensagemSucesso = `Tudo certo!`;
 
-    FormEValido = validaNumero(NumeroB.value)
-if (FormEValido) {
-    alert(mensagemSucesso);
+    if (NumeroB.value > NumeroA.value) {
+        alert(mensagemSucesso);
 
-    NumeroA.value = '';
-    NumeroB.value = '';
-} else {
-    alert('O segundo número precisa ser maior que o primeiro');
+        NumeroA.value = '';
+        NumeroB.value = '';
+    } else {
+        alert("O segundo número precisa ser maior que o primeiro");
     }
 })
 
-console.log (form)
+console.log(form);
